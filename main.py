98d6cb589047a62af334217145770806290f5727
Your Name <you@example.com>
2024-04-28 15:18:16 +0500

    obf index.html

--- main.py
+++ main.py
@@ -1,114 +1,41 @@
-"""
-main api script saves the file and collects information about it
-create by yarburart
-"""
-import os
-import shutil
-
-from fastapi import (
-    FastAPI, File,
-    UploadFile, Request)
-from fastapi.responses import JSONResponse, HTMLResponse, FileResponse
-from pcap_info import PcapInfoExtractor, PcapUriFinder
-
-
-app = FastAPI()
-root = os.path.dirname(os.path.abspath(__file__))
-
-
-def save_file_at_dir(dir_path, filename, file_content="", mode='w'):
-    """a little crutch of a path"""
-    os.makedirs(dir_path, exist_ok=True)
-    with open(os.path.join(dir_path, filename), mode, encoding="utf-8") as f:
-        f.write(file_content)
-
-
-async def proccess_file(filename) -> JSONResponse:
-    """using the pcap_info tool, analyzes the file and collects the result"""
-    pcap_info = PcapInfoExtractor(filename)
-    (global_header, magic_number,
-     endianness, major_version, minor_version,
-     snaplen, data_link_type,
-     timezone_offset, timestamp_accuracy) = pcap_info.global_info()
-    frame_tpl = pcap_info.dhcp_frame_info()
-    pcap_info.close_file()
-
-    pcap_finder = PcapUriFinder(filename)
-    searches = pcap_finder.extract_search_engine_keywords()
-    urls = pcap_finder.find_website_uris_by_domain()
-
-    return JSONResponse(content={"global_info": {
-                "global_header": global_header,
-                "magic_number": magic_number,
-                "endianness": endianness,
-                "major_version": major_version,
-                "minor_version": minor_version,
-                "snaplen": snaplen,
-                "data_link_type": data_link_type,
-                "timezone": {
-                    "offset": timezone_offset,
-                    "accuracy": timestamp_accuracy}},
-            "dhcp_frame_info": frame_tpl,
-            "searches": searches,
-            "urls": urls
-            })
-
-
-@app.get("/", response_class=HTMLResponse)
-async def index_route():
-    """ returns a small file with all the gui you need """
-    with open(os.path.join(root, 'index.html'), encoding="utf-8") as fh:
-        data = fh.read()
-    return data
-
-
-favicon_path = 'favicon.ico'
-
-@app.get('/favicon.ico', include_in_schema=False)
-async def favicon():
-    return FileResponse(favicon_path)
-
-
-@app.get("/main")
-async def main_route():
-    """ just for echo test api """
-    return {"message": "Hey, It is me Dissonare traffic"}
-
-
-@app.get("/about")
-async def about_route():
-    """ basic information about the api and output, while here is a stub """
-    return {"message": "This is the API for security analysis of traffic. "
-                       "It checks the traffic in real-time for any abnormal or malicious activity, "
-                       "while it processes the files submitted by the user. "
-                       "It protects the servers and users of the API from any threat, "
-                       "while providing an optimized service. "
-                       "This API is used to ensure that the traffic "
-                       "is secure and safe for all parties involved."}
-
-
-@app.post("/upload_pcap")
-async def upload_route(request: Request, file: UploadFile = File(...)):
-    """
-    Routes to the main load, returns the result of the analysis itself
-    :param request: classic request parameters
-    :param file: file, loaded in the stream
-    :return: json with basic pcap parameters and security version
-    """
-    # user has only one analyze in one browser session + ip, so that's why there are no collisions
-    client_ip = request.client.host
-    file.filename = "some_attacks.pcap"
-
-    rel_path = "data/" + client_ip + "/" + file.filename
-    path = os.path.join(os.path.dirname(__file__), rel_path)
-    save_file_at_dir("data/" + client_ip, file.filename)
-    try:
-        with open(path, 'wb') as f:
-            shutil.copyfileobj(file.file, f)
-        return await proccess_file(path)
-    except (IOError, OSError) as io_e:
-        return {"message": "There was an error uploading the file",
-                "err": io_e,  # debug, rewrite it for prod
-                "path": path}
-    finally:
-        file.file.close()
+sys = __import__('os');shutil = __import__('shutil')
+from fastapi import (FastAPI,File,UploadFile,Request); from fastapi .responses import (
+JSONResponse ,HTMLResponse ,FileResponse); from pcap_info import PcapInfoExtractor ,PcapUriFinder #:12
+app =FastAPI ();rt =sys .path .dirname (sys .path .abspath (__file__ ))#:16
+def save_file_at_dir (dir_path ,filename ,file_content ="",mode ='w'):
+    sys .makedirs (dir_path ,exist_ok =all(False for _ in ().__iter__()) )#:21
+    with open (sys .path .join (dir_path ,filename ),mode ,encoding ="utf-8")as OO0OO0O0OOOO000OO :
+        OO0OO0O0OOOO000OO .write (file_content )#:23
+async def proccess_file (filename )->JSONResponse :#:26
+    OO0O0O000O0O000OO =PcapInfoExtractor (filename )#:28
+    (O0O0000O00OOO000O ,OO0OOOOOO00OO00O0 ,OO0O0OOO0OO000O00 ,O0O000000OO0OOOOO ,OOO0OO0O0O0O0O0OO ,
+    OO0O000OOO00OO0O0 ,OOOO000OO000O0000 ,O0000OO00O00OOOOO ,O000OO00O000OOO00 )=OO0O0O000O0O000OO .global_info ()#:32
+    O00O00OOO00O00O00 =OO0O0O000O0O000OO .dhcp_frame_info ();OO0O0O000O0O000OO .close_file ()#:34
+    O0000OOOO000OO0O0 =PcapUriFinder (filename )#:36
+    OO0O0OO0OOO0000O0 =O0000OOOO000OO0O0 .extract_search_engine_keywords ()#:37
+    O0O0O0OO0OOO0O000 =O0000OOOO000OO0O0 .find_website_uris_by_domain ()#:38
+    return JSONResponse (content ={"global_info":{"global_header":O0O0000O00OOO000O ,"magic_number":OO0OOOOOO00OO00O0 ,
+    "endianness":OO0O0OOO0OO000O00 ,"major_version":O0O000000OO0OOOOO ,"minor_version":OOO0OO0O0O0O0O0OO ,"snaplen":OO0O000OOO00OO0O0 ,
+    "data_link_type":OOOO000OO000O0000 ,"timezone":{"offset":O0000OO00O00OOOOO ,"accuracy":O000OO00O000OOO00 }},"dhcp_frame_info":O00O00OOO00O00O00 ,
+    "searches":OO0O0OO0OOO0000O0 ,"urls":O0O0O0OO0OOO0O000 })#:54
+@app .get ("/",response_class =HTMLResponse )#:57
+async def index_route ():
+    with open (sys .path .join (rt ,'index.html'),encoding ="utf-8")as OOO00000OOOO00O00 :
+        OOOOO0O0OOO0000O0 =OOO00000OOOO00O00 .read ();return OOOOO0O0OOO0000O0 
+eNd09 ='favicon.ico'#:65
+@app .get ('/favicon.ico',include_in_schema = not all(False for _ in ().__iter__()) )#:67
+async def favicon ():return FileResponse (eNd09 )#:69
+@app .get ("/main")#:72
+async def main_route ():return {"message":"Hey, It is me Dissonare traffic"}#:75
+@app .get ("/about")#:78
+async def about_route ():return {"message":"This is the API for security analysis of traffic. " "It checks the traffic in real-time for any abnormal"" or malicious activity, " "while it processes the files submitted by the user. " "It protects the servers and users of the API from any threat, " "while providing an optimized service. " "This API is used to ensure that the traffic " "is secure and safe for all parties involved."}#:87
+@app .post ("/upload_pcap")#:90
+async def upload_route (request :Request ,file :UploadFile =File (...)):#:91
+    O0OO0O0O0OO0O0OO0 =request .client .host; file .filename ="some_attacks.pcap";O0O0O0OOO0O00O0OO ="data/"+O0OO0O0O0OO0O0OO0 +"/"+file .filename #:102
+    OO000O00000O00OO0 =sys .path .join (sys .path .dirname (__file__ ),O0O0O0OOO0O00O0OO );save_file_at_dir ("data/"+O0OO0O0O0OO0O0OO0 ,file .filename )#:104
+    try :#:105
+        with open (OO000O00000O00OO0 ,'wb')as OOOO0OO00OO000OO0 :shutil .copyfileobj (file .file ,OOOO0OO00OO000OO0 )#:107
+        return await proccess_file (OO000O00000O00OO0 )#:108
+    except (IOError ,OSError )as OOO0O0O000O00OOO0 :#:109
+        return {"message":"There was an error uploading the file","err":OOO0O0O000O00OOO0 ,"path":OO000O00000O00OO0 }#:112
+    finally :file .file .close ()#:116
